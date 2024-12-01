8e47cc24cb7da8bf0d95e7920621422eeaf9b86e
Isaac-Enriquez <isaac.enriquez@softpital.com>
2024-12-01 02:26:38 -0600

    Commit final?

--- build/tests/CTestTestfile.cmake
+++ build/tests/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(test_buscar_patron "C:/Users/Shaque/Documents/GitHub/Evidencia-algoritmos/build/test_runner.exe")
-set_tests_properties(test_buscar_patron PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Shaque/Documents/GitHub/Evidencia-algoritmos/tests/CMakeLists.txt;14;add_test;C:/Users/Shaque/Documents/GitHub/Evidencia-algoritmos/tests/CMakeLists.txt;0;")
+add_test(TestBuscarPatron "C:/Users/Shaque/Documents/GitHub/Evidencia-algoritmos/build/test_runner.exe")
+set_tests_properties(TestBuscarPatron PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Shaque/Documents/GitHub/Evidencia-algoritmos/tests/CMakeLists.txt;24;add_test;C:/Users/Shaque/Documents/GitHub/Evidencia-algoritmos/tests/CMakeLists.txt;0;")
